3064a4c3c3502aa2f73eb1cb6f35a8288fc7e267
Julie Chabanis <98469515+JulieChabanis@users.noreply.github.com>
2023-01-24 16:53:51 +0000

    Add Main class

---
+++ Main.java
@@ -0,0 +1,6 @@
+public static void main(String[] args) {
+		Empreinte e = new Empreinte();
+        e.test("motDePasseTest");
+        e.test("motDePasseTest");
+        e.test("motDePasseTest1");
+	}
